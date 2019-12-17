1e619383d71b04d3b0311ea964fa2b7909667aff
nangpham26 <54834355+nangpham26@users.noreply.github.com>
2019-12-17 10:24:41 +0700

    commit

--- blogacademic/index.php
+++ blogacademic/index.php
@@ -21,12 +21,18 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.1/css/all.css"></link>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<script type="js/script.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/swiper.min.css"/>
+		<link rel="stylesheet" type="text/css" href="connection.css">
+		<script src="js/script.js"></script>
+		<script src="js/swiper.min.js"></script>
+    	<script src="js/jquery.min.js"></script>
+    	<script src="js/2.js"></script>
     </head>
     <body>
     	
     	<div id="all-world" class="blog-world">
     		<?php include './header.php' ?>
+    		<?php include './module/2-content.php' ?>
     	</div>
     	
 
---
+++ blogacademic/module/2-content.php
@@ -0,0 +1,79 @@
+
+<div class="type-2">
+    <div class="header container-fluid">
+        <div class="header-slideshow swiper-container">
+            <div class="slideshow swiper-wrapper">
+                <div class="swiper-slide" id="slide-1">
+                    <div class="container">
+                        <div class="slider-content">
+                            <a href="#" tabindex="-1">
+                                <h2 class="title">Học viên học ngoại khoá</h2></a>
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod&nbsp;tempor incididunt ut labore et…</p>
+                                <a href="#" class="button-content" tabindex="-1">Read More
+                                    <i class="fa fa-angle-right"></i>
+                                </a>   
+                        </div><!-- end .main-slider-contents -->
+                    </div>
+                </div>
+                <div class="swiper-slide" id="slide-2">
+                    <div class="slider-content">
+                        <div class="container">
+                            <div class="slider-content">
+                                <a href="#" tabindex="-1">
+                                <h2 class="title">Thư viện</h2></a>
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod&nbsp;tempor incididunt ut labore et…</p>
+                                <a href="#" class="button-content" tabindex="-1">Read More
+                                    <i class="fa fa-angle-right"></i>
+                                </a>   
+                            </div><!-- end .main-slider-contents -->
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide" id="slide-3">
+                    <div class="slider-content">
+                        <div class="container">
+                            <div class="slider-content">
+                                <a href="#" tabindex="-1">
+                                <h2 class="title">Nghiên cứu</h2></a>
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod&nbsp;tempor incididunt ut labore et…</p>
+                                <a href="#" class="button-content" tabindex="-1">Read More
+                                    <i class="fa fa-angle-right"></i>
+                                </a>   
+                            </div><!-- end .main-slider-contents -->
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide" id="slide-4">
+                    <div class="slider-content">
+                        <div class="container">
+                            <div class="slider-content">
+                                <a href="#" tabindex="-1">
+                                <h2 class="title">Học viên tốt nghiệp </h2></a>
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod&nbsp;tempor incididunt ut labore et…</p>
+                                <a href="#" class="button-content" tabindex="-1">Read More
+                                    <i class="fa fa-angle-right"></i>
+                                </a>   
+                            </div><!-- end .main-slider-contents -->
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide" id="slide-5">
+                    <div class="slider-content">
+                        <div class="container">
+                            <div class="slider-content">
+                                <a href="#" tabindex="-1">
+                                <h2 class="title">Luyện thi</h2></a>
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod&nbsp;tempor incididunt ut labore et…</p>
+                                <a href="#" class="button-content" tabindex="-1">Read More
+                                    <i class="fa fa-angle-right"></i>
+                                </a>   
+                            </div><!-- end .main-slider-contents -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+</div>
