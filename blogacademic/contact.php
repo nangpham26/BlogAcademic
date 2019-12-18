c3784fb16978ab505859b5a6a80168fe43a804f2
quochoa1999 <lehoa422@gmail.com>
2019-12-18 17:55:03 +0700

    commit

---
+++ blogacademic/contact.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/contact.css">
+</head>
+
+<body>
+    <div id="all-world" class="blog-world">
+        <?php include './header.php' ?>
+        <div class="main">
+            
+            <div class="main-middle container">
+                <h1 class="title-main">Liên hệ</h1>
+                <h1 class="contacts">Thông tin liên hệ:</h1>
+                <p>Văn phòng làm việc:
+                    Tp.HCM.<br>
+                    Điểm tiếp nhận:
+                    Tp.HCM.<br>
+                    Điện thoại: 0343546369. <br>
+                    Email: lehoa422@gmail.com.<br>
+                    Zalo: 0343546369. <br>
+                    Facebook Fanpage: fb.com/quochoa.1999
+                </p>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15673.3371416394!2d106.75884969999998!3d10.862159199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752797e321f8e9%3A0xb3ff69197b10ec4f!2zVHLGsOG7nW5nIENhbyDEkOG6s25nIEPDtG5nIG5naOG7hyBUaOG7pyDEkOG7qWM!5e0!3m2!1svi!2s!4v1576663787438!5m2!1svi!2s" 
+                width="90%" height="30%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            </div>
+            
+        </div>
+    </div>
+
+</body>
+
+</html>
