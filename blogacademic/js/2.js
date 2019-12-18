8c1fb2f838cceefc723ece44b8ca8afc718c2006
quochoa1999 <lehoa422@gmail.com>
2019-12-18 16:33:42 +0700

    commit

--- blogacademic/js/2.js
+++ blogacademic/js/2.js
@@ -1,67 +1,76 @@
 $(document).ready(function() {
-  var swiper = new Swiper(".header-slideshow", {
+  var headerslideshow = new Swiper(".header-slideshow", {
     effect: "fade",
-  	slidesPerView: 1,
+    slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
     autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+      delay: 2500,
+      disableOnInteraction: false
+    },
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
+      clickable: true
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
   });
-});
-$(document).ready(function() {
+
   var gallerybutton = new Swiper(".gallery-button", {
+    navigation: {
+      nextEl: ".gallery-next",
+      prevEl: ".gallery-prev"
+    },
     slidesPerView: 6,
     spaceBetween: 10,
     slidesPerGroup: 1,
     loop: true,
     loopFillGroupWithBlank: true,
-    navigation: {
-      nextEl: '#swiper_btn_next',
-      prevEl: '#swiper_btn_prev',
-    }, 
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+    }
+    
   });
   var latestnews = new Swiper(".latest-news", {
+    navigation: {
+      nextEl: ".late-next",
+      prevEl: ".late-prev"
+    },
     slidesPerView: 4,
     spaceBetween: 10,
     slidesPerGroup: 1,
     loop: true,
     loopFillGroupWithBlank: true,
-    navigation: {
-      nextEl: '#swiper_btn_next',
-      prevEl: '#swiper_btn_prev',
-    }, 
+    
   });
   var events = new Swiper(".events", {
+    navigation: {
+      nextEl: ".event-next",
+      prevEl: ".event-prev"
+    },
     slidesPerView: 3,
     spaceBetween: 10,
     slidesPerGroup: 1,
     loop: true,
     loopFillGroupWithBlank: true,
-    navigation: {
-      nextEl: '#swiper_btn_next',
-      prevEl: '#swiper_btn_prev',
-    }, 
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+    }
   });
   var partners = new Swiper(".partners", {
+    navigation: {
+      nextEl: ".partners-next",
+      prevEl: ".partners-prev"
+    },
     slidesPerView: 6,
     spaceBetween: 10,
     slidesPerGroup: 1,
     loop: true,
     loopFillGroupWithBlank: true,
-    navigation: {
-      nextEl: '#cc1',
-      prevEl: '#cc',
-    }, 
+    
   });
 });
-
