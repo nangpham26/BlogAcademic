7b8c046f5e69be0a6df837d5d040d0da2933069e
quochoa1999 <lehoa422@gmail.com>
2019-12-26 00:09:23 +0700

    css
    huongdanhoc

--- blogacademic/huongdanhoc.php
+++ blogacademic/huongdanhoc.php
@@ -9,12 +9,165 @@
                     </div>
                 </div>
             </div>
-            <div class="container">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+            <div class="middle container">
+        <div class="column-left">
+            <div class="column-post">
+                <div class="khung">
+                    <img src="./img/9-1.png" alt="">
+                    <div class="ndung">
+                        <h3>Nothing Beats New York</h3>
+                        <h5>January 23, 2018</h5>
+                        <p>Etiam placerat velit vitae dui blandit sollicitudin. Vestibulum tincidunt sed dolor sit amet volutpat. Nullam egestas sem at mollis sodales. Nunc eget lacinia eros,…</p>
+                        <div class="ndung-buttons">
+                            <a href="#" class="button-a-post">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column-post">
+                <div class="khung">
+                    <img src="./img/9-1.png" alt="">
+                    <div class="ndung">
+                        <h3>Nothing Beats New York</h3>
+                        <h5>January 23, 2018</h5>
+                        <p>Etiam placerat velit vitae dui blandit sollicitudin. Vestibulum tincidunt sed dolor sit amet volutpat. Nullam egestas sem at mollis sodales. Nunc eget lacinia eros,…</p>
+                        <div class="ndung-buttons">
+                            <a href="#" class="button-a-post">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column-post">
+                <div class="khung">
+                    <img src="./img/9-2.png" alt="">
+                    <div class="ndung">
+                        <h3>Nothing Beats New York</h3>
+                        <h5>January 23, 2018</h5>
+                        <p>Etiam placerat velit vitae dui blandit sollicitudin. Vestibulum tincidunt sed dolor sit amet volutpat. Nullam egestas sem at mollis sodales. Nunc eget lacinia eros,…</p>
+                        <div class="ndung-buttons">
+                            <a href="#" class="button-a-post">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column-post">
+                <div class="khung">
+                    <img src="./img/9-3.png" alt="">
+                    <div class="ndung">
+                        <h3>Nothing Beats New York</h3>
+                        <h5>January 23, 2018</h5>
+                        <p>Etiam placerat velit vitae dui blandit sollicitudin. Vestibulum tincidunt sed dolor sit amet volutpat. Nullam egestas sem at mollis sodales. Nunc eget lacinia eros,…</p>
+                        <div class="ndung-buttons">
+                            <a href="#" class="button-a-post">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column-post">
+                <div class="khung">
+                    <img src="./img/9-4.png" alt="">
+                    <div class="ndung">
+                        <h3>Nothing Beats New York</h3>
+                        <h5>January 23, 2018</h5>
+                        <p>Etiam placerat velit vitae dui blandit sollicitudin. Vestibulum tincidunt sed dolor sit amet volutpat. Nullam egestas sem at mollis sodales. Nunc eget lacinia eros,…</p>
+                        <div class="ndung-buttons">
+                            <a href="#" class="button-a-post">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="text-center">
+                <div class="item-list">
+                    <ul class="pagination">
+                        <li class="page"><a href="#">1</a></li>
+                        <li class="page"><a title="Go to page 2" href="#">2</a></li>
+                        <li class="page"><a title="Go to next page" href="#">Next</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="column-right">
+            <h4 class="widget-title">Looking for something?</h4>
+            <section class="search-form">
+                <form method="get" class="product-search" action="#">
+                    <input type="search" class="search-field" placeholder="Search…" value="" name="s">
+                    <button type="submit" value="Search">Search</button>
+                    <input type="hidden" name="post_type" value="product">
+                </form>
+            </section>
+            <div class="items">
+                <div class="line-item">
+                    <div class="img-item">
+                        <img src="./img/1.jpg" alt="hinh" class="img-responsive">
+                    </div>
+                    <div class="img-content">
+                        <a class="sub-title">Anthropology Open House</a><br>
+                        <span class="des"><i class="far fa-clock"></i>
+                            February 8, 2018
+                        </span>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="line-item">
+                    <div class="img-item">
+                        <img src="./img/1.jpg" alt="hinh" class="img-responsive">
+                    </div>
+                    <div class="img-content">
+                        <a class="sub-title">Anthropology Open House</a><br>
+                        <span class="des"><i class="far fa-clock"></i>
+                            February 8, 2018
+                        </span>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="line-item">
+                    <div class="img-item">
+                        <img src="./img/1.jpg" alt="hinh" class="img-responsive">
+                    </div>
+                    <div class="img-content">
+                        <a class="sub-title">Anthropology Open House</a><br>
+                        <span class="des"><i class="far fa-clock"></i>
+                            February 8, 2018
+                        </span>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="line-item">
+                    <div class="img-item">
+                        <img src="./img/1.jpg" alt="hinh" class="img-responsive">
+                    </div>
+                    <div class="img-content">
+                        <a class="sub-title">Anthropology Open House</a><br>
+                        <span class="des"><i class="far fa-clock"></i>
+                            February 8, 2018
+                        </span>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="line-item">
+                    <div class="img-item">
+                        <img src="./img/1.jpg" alt="hinh" class="img-responsive">
+                    </div>
+                    <div class="img-content">
+                        <a class="sub-title">Anthropology Open House</a><br>
+                        <span class="des"><i class="far fa-clock"></i>
+                            February 8, 2018
+                        </span>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+            <section class="forlow-title">
+                <h4 class="widget-title">Follow us on Social Media</h4>
+                <a href="#"><img src="./img/twitter.png" alt="" class="imgresponsive"></a>
+                <a href="#"><img src="./img/facebook.png" alt="" class="imgresponsive"></a>
+                <a href="#"><img src="./img/instagram.png" alt="" class="imgresponsive"></a>
+                <a href="#"><img src="./img/youtube.png" alt="" class="imgresponsive"></a>
+            </section>
+
+        </div>
+    </div>
     </div>
     <?php include './footer.php'; ?>
 </body>
